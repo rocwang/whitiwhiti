0716c9cd6906d0cea0deb1e1fd6d28d10aa6d52b
Roc Wong <roc.wong@vitaler.com>
2021-05-19 22:56:45 +1200

    feat: prototype
    Signed-off-by: Roc Wong <roc.wong@vitaler.com>

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,151 @@
 <template>
-  <p>shift swap</p>
+  <div :class="$style.candidates">
+    <div :class="$style.item" v-for="item in candidates">
+      <p>{{ item.user.summary }}</p>
+      <p>{{ format(item.start) }} - {{ format(item.end) }}</p>
+    </div>
+    =>
+    <div :class="$style.item" v-for="item in swapped">
+      <p>{{ item.user.summary }}</p>
+      <p>{{ format(item.start) }} - {{ format(item.end) }}</p>
+    </div>
+
+    <button type="button">Swap</button>
+  </div>
+
+  <div :class="$style.grid">
+    <div :class="$style.item" v-for="item in onCalls" @click="handleItem(item)">
+      <p>{{ item.user.summary }}</p>
+      <p>{{ format(item.start) }} - {{ format(item.end) }}</p>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent, onMounted, ref } from "vue";
+import { api } from "@pagerduty/pdjs";
+import { APIParameters } from "@pagerduty/pdjs/build/src/api";
+import {
+  addDays,
+  formatISO,
+  formatWithOptions,
+  parseISO
+} from "date-fns/esm/fp";
+import { pipe } from "ramda";
+import { enNZ } from "date-fns/locale";
 
 export default defineComponent({
-  name: 'App',
-  components: {
+  name: "App",
+  components: {},
+  setup() {
+    const timeZone = "Pacific/Auckland";
+    const token = window.localStorage.getItem("token");
+    const pd = api({ token, tokenType: "bearer" } as Partial<APIParameters>);
+
+    const onCalls = ref([]);
+    onMounted(async () => {
+      const { resource } = await pd.get("/oncalls", {
+        data: {
+          time_zone: timeZone,
+          limit: 100,
+          until: formatISO(addDays(90, new Date()))
+        }
+      });
+
+      onCalls.value = resource.filter(onCall => onCall.schedule);
+    });
+
+    const format = pipe(
+      parseISO,
+      formatWithOptions({ weekStartsOn: 1, locale: enNZ }, "Pp")
+    );
+
+    const candidates = ref([]);
+
+    function handleItem(clickedItem: unknown): void {
+      if (candidates.value.includes(clickedItem)) {
+        candidates.value = candidates.value.filter(
+          item => item !== clickedItem
+        );
+      } else {
+        candidates.value = [...candidates.value.slice(-1), clickedItem];
+      }
+    }
+
+    const swapped = computed(() => {
+      if (candidates.value.length === 2) {
+        const c0 = candidates.value[0];
+        const c1 = candidates.value[1];
+        const c0Start = c0.start;
+        const c0End = c0.end;
+
+        return [
+          { ...c1, start: c0Start, end: c0End },
+          { ...c0, start: c1.start, end: c1.end }
+        ];
+      } else {
+        return [];
+      }
+    });
+
+    async function swap() {
+      const c = candidates.value;
+      const s = swapped.value;
+
+      const promises = [0, 1].map(i =>
+        pd.post(`/schedules/${c[i].schedule.id}/overrides`, {
+          data: {
+            overrides: [
+              {
+                start: s[i].start,
+                end: s[i].end,
+                user: s[i].user,
+                time_zone: timeZone
+              }
+            ]
+          }
+        })
+      );
+
+      const results = await Promise.allSettled(promises);
+      if (results.every(r => (r.status = "fulfilled"))) {
+        alert("Done!");
+      } else {
+        alert("Failed to swap!");
+      }
+    }
+
+    return {
+      candidates,
+      format,
+      handleItem,
+      onCalls,
+      swapped,
+      swap,
+      token
+    };
   }
-})
+});
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style module>
+.candidates {
+  position: sticky;
+  top: 0;
+  z-index: 10;
+  background-color: #fff;
+  display: flex;
+  flex-flow: row nowrap;
+  border: 1px solid #000;
+  justify-content: space-between;
+}
+
+.grid {
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: repeat(5, 1fr);
+}
+
+.item {
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,62 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/oauth-callback",
+      component: App,
+      beforeEnter: async () => {
+        const urlParams = new URLSearchParams(location.search);
+        const code = urlParams.get("code");
+
+        const response = await fetch(
+          `https://app.pagerduty.com/oauth/token?grant_type=authorization_code&client_id=${
+            import.meta.env.VITE_CLIENT_ID
+          }&redirect_uri=${
+            import.meta.env.VITE_REDIRECT_URI
+          }&code=${code}&code_verifier`,
+          {
+            method: "POST"
+          }
+        );
+
+        if (response.ok) {
+          const json = await response.json();
+
+          if (json.access_token) {
+            localStorage.setItem("token", json.access_token);
+          }
+        }
+
+        return "/";
+      }
+    },
+    {
+      path: "/",
+      name: "home",
+      component: App,
+      beforeEnter: () => {
+        const token = localStorage.getItem("token");
+
+        if (token) {
+          return true;
+        } else {
+          location.href = `https://app.pagerduty.com/oauth/authorize?client_id=${
+            import.meta.env.VITE_CLIENT_ID
+          }&redirect_uri=${
+            import.meta.env.VITE_REDIRECT_URI
+          }&response_type=code&code_challenge_method=S256&code_challenge`;
+
+          return false;
+        }
+      }
+    }
+  ]
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
